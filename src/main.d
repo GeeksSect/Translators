dcc89f8ab9012367b51b0a4c1b64881036fcf76c
Artemenko Vladimir <artemenko.vladimi@gmail.com>
2015-04-16 21:15:10 +0300

    First step in creating assembler

--- src/main.d
+++ src/main.d
@@ -1,6 +1,26 @@
 module main;
 
+import std.stdio;
+import std.string;
+
+import spec.common;
+
 int main(string[] args)
 {
+	File file = File(args[1], "r");
+	char[][] wordsLine;
+	string line;
+
+	while (!file.eof()) {
+		line = chomp(file.readln);
+		if (line.length) {
+			wordsLine = split(line.dup);
+			foreach (char[] curWord; wordsLine) {
+				write('[', curWord, ']', ' ');
+			}
+			writeln();
+		}
+	}
+
 	return 0;
 }
